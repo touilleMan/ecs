f9ab78b03fde62651e4b14184d27cf854601add8
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-12-13 11:29:57 +0100

    Use __all__ in __init__.py

--- ecs/__init__.py
+++ ecs/__init__.py
@@ -2,11 +2,14 @@
 
 from ecs import metadata as _metadata
 # Provide a common namespace for these classes.
-from ecs.models import Entity, Component, System  # NOQA
-from ecs.managers import EntityManager, SystemManager  # NOQA
+from ecs.models import Entity, Component, System
+from ecs.managers import EntityManager, SystemManager
 
 
 __version__ = _metadata.version
 __author__ = _metadata.authors[0]
 __license__ = _metadata.license
 __copyright__ = _metadata.copyright
+
+
+__all__ = ['Entity', 'Component', 'System', 'EntityManager', 'SystemManager']
